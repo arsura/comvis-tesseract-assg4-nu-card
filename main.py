d5d0f83e4a7fb77fdfe4bc818d05426477622adc
Siwakorn <siwakornr58@email.nu.ac.th>
2018-11-03 01:43:56 +0700

    sleepy commit

--- main.py
+++ main.py
@@ -5,24 +5,39 @@ import numpy as np
 from dir import *
 from read_write import read_line_to_list, write_line
 
+# cv.namedWindow("windows", cv.WINDOW_NORMAL)
+
 def tesseract_cmd(src_path, image, des_path, lang):
     os.system("tesseract {}{} {}{} -l {} -c preserve_interword_spaces=1".format(src_path, image, des_path, image, lang)) 
 
 def dont_equal_frame_screen(src_w, src_h, current_w, current_h):
     return src_h != current_h and src_w != current_w
 
+def find_id(data):
+    index = 0
+    for item in data:
+        if ((len(str(item)) == 8) and 
+            (item[0] == "5") and 
+            (item[1] == "8")):
+            break
+        index += 1 
+    if (index >= len(data)):
+        return -1
+    return index
+
 def main():
-    cap = cv.VideoCapture(SRC_DIR)
-    while(cap.isOpened()):
+    cap = cv.VideoCapture(0)
+    while(True):
+
         frame_ret, frame = cap.read()
         frame_height, frame_width = frame.shape[:2]
 
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         thrsh_ret, threshold_frame = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)
-        thrsh_for_text_ret, threshold_frame_for_text = cv.threshold(gray, 25, 255, cv.THRESH_BINARY)
+        thrsh_for_text_ret, threshold_frame_for_text = cv.threshold(gray, 20, 255, cv.THRESH_BINARY)
 
         contours, hierarchy = cv.findContours(threshold_frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        max_h = max_w = 1
+        max_x = max_y = max_w = max_h = 1
 
         for contour in contours:
             x, y, w, h = cv.boundingRect(contour)
@@ -39,7 +54,7 @@ def main():
             "wide": (max_x + max_w)
         }
 
-        # cv.rectangle(threshold_frame_for_text, (crop_size["x"], crop_size["y"]), (crop_size["wide"], crop_size["height"]), (0, 255, 0), 10)
+        cv.rectangle(frame, (crop_size["x"], crop_size["y"]), (crop_size["wide"], crop_size["height"]), (0, 255, 0), 5)
 
         crop_img = threshold_frame_for_text[crop_size["y"]:crop_size["height"], crop_size["x"]:crop_size["wide"]]
         crop_img = cv.resize(crop_img, None, fx=0.7, fy=0.7)
@@ -53,17 +68,23 @@ def main():
 
         # read data
         nu_card_data = read_line_to_list(TESSERACT_OUTPUT_DIR + "{}_crop_thrs_img.jpg.txt".format(time_now))
-        university_name = nu_card_data[0]
-        name = nu_card_data[2]
-        nu_id = nu_card_data[4]
-        new_nu_card_data_list = [name, nu_id]
+        index_id = find_id(nu_card_data)
+
+        if (index_id == -1): # Not Found ID
+            print("Not Found ID")
+            continue
+
+        nu_id = nu_card_data[index_id]
+        name = nu_card_data[index_id - 2]
+        new_nu_card_data = [nu_id, name]
 
         # write data
         data_name = "{}_{}.txt".format(time_now, nu_id)
-        write_line(NU_CARD_DATA, data_name, new_nu_card_data_list)
+        write_line(NU_CARD_DATA, data_name, new_nu_card_data)
 
         # validate
 
+        cv.imshow("windows", frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
